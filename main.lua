800d478daf05aa8a0cbc3d4bcbf5348baea113b4
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2026-02-02 23:26:12 +0000

    AI go, make it work, then I make it pretty later

--- main.lua
+++ main.lua
@@ -1,137 +1,215 @@
--- local name,addon=...;
--- addon.myvar = "test"
-
-local MY_CHAR_UNIT = "player"
-local MY_RAID_TARGET_INDEX = 1
-local OTHER_CHAR_UNIT = "Addonmanager"
-local OTHER_RAID_TARGET_INDEX = 3
-
-
-local optedOut = GetOptOutOfLoot()
-print(optedOut)
-
-
+-- FFAParty.lua
+-- main.lua
+local addonName, addon = ...
+addon = addon or {}
+_G[addonName] = addon
+print("FFA Party: main.lua loaded for", addonName, "addon table:", addon)
 
 local f = CreateFrame("Frame")
 
-function f:OnEvent(event, ...)
-    self[event](self, event, ...)
+------------------------------------------------------------
+-- Default settings
+------------------------------------------------------------
+local defaults = {
+    lootWithFriends = "freeforall",
+    lootWithOthers = "group",
+    ignoreRaids = true,
+    showMessages = true,
+    debug = false,
+    customFriends = {} -- manual whitelist
+}
+
+------------------------------------------------------------
+-- Loot method mapping
+------------------------------------------------------------
+local lootMethodMap = {
+    freeforall = 0,
+    roundrobin = 1,
+    master = 2,
+    group = 3,
+    needbeforegreed = 4
+}
+
+------------------------------------------------------------
+-- Utility: normalize names (strip realm if present)
+------------------------------------------------------------
+local function NormalizeName(name)
+    if not name then
+        return nil
+    end
+    local base = name:match("^(.-)%-.+$")
+    return base or name
 end
 
-function f:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
-    -- SetOptOutOfLoot(false)
-    -- print(event, isLogin, isReload)
+------------------------------------------------------------
+-- Debug print helper
+------------------------------------------------------------
+local function DebugPrint(msg)
+    -- if FFAPartyDB and FFAPartyDB.debug then
+    print("|cff00ff00[FFA Party DEBUG]|r " .. msg)
+    -- end
 end
 
-
-
--- TODO:
--- // only attempt actions if we are the group leader
--- also use PLAYER_LOGIN / PLAYER_ENTERING_WORLD to init everything in case we are the party leader but were offline/on a loading screen
--- if group leader changes:
---   check group type
---     if we are leader, update as expected
---     if we are not leader, warn that the type is not as expected?
---       ? maybe make this an option
---     if we are in a raid, do not change lootmethod if others are in the party
-
-
-local LOOT_TYPE_FFA = "freeforall"
-local LOOT_TYPE_GROUP = "group"
-
-local function table_has_value (tab, val)
-    for _, value in pairs(tab) do
-        if value == val then
-            return true
-        end
-    end
-
-    return false
-end
-
-local function ensure_raid_target(unit, raidTargetIndex)
-    if GetRaidTargetIndex(unit) ~= raidTargetIndex then
-        SetRaidTarget(unit, raidTargetIndex)
-    end
-end
-
-local function ensure_loot_method(lootMethod)
-    if GetLootMethod() ~= lootMethod then
-        print("Setting group loot")
-        SetLootMethod(lootMethod)
-    end
-end
-
-local function process_group_loot_type()
-    -- local currentCharacterName = UnitName("player")
-    -- local allowedCharacterNames = { "Boboboy" }
-    local allowedCharacterNames = { OTHER_CHAR_UNIT }
-    -- local currentGroupCharacterNames = { currentCharacterName, "Boboboy", "123" }
-    local currentGroupCharacterNames = GetHomePartyInfo()
-
-    local inGroup = IsInGroup()
-    if inGroup ~= true then
-        -- print("No actions taken, not in group")
-        return
-    end
-    
-    local isLeader = UnitIsGroupLeader("player")
-    if isLeader ~= true then
-        -- print("No actions taken, not group leader")
-        return
-    end
-
-    local disallowedCharacterInGroup = false
-    for _, v in pairs(currentGroupCharacterNames) do
-        if table_has_value(allowedCharacterNames, v) then
-            --
-        else
-            disallowedCharacterInGroup = true
-            break
-        end
-    end
-
-    if disallowedCharacterInGroup then
-        local isInRaid = IsInRaid()
-        if isInRaid then
-            local currentLootMethod = GetLootMethod()
-            if currentLootMethod == LOOT_TYPE_FFA then
-                print("In raid and loot type is FFA, you might want to switch to something else")
-                -- print("In raid and loot type is FFA, switching to group loot")
-                -- SetLootMethod(LOOT_TYPE_GROUP)
+------------------------------------------------------------
+-- Collect WoW friends
+------------------------------------------------------------
+local function GetWoWFriends()
+    local friends = {}
+    if C_FriendList and C_FriendList.GetNumFriends then
+        for i = 1, C_FriendList.GetNumFriends() do
+            local info = C_FriendList.GetFriendInfoByIndex(i)
+            if info and info.name then
+                friends[NormalizeName(info.name)] = true
             end
         end
+    end
+    return friends
+end
 
-        ensure_loot_method(LOOT_TYPE_GROUP)
+------------------------------------------------------------
+-- Collect Battle.net friends (not available in TBC Classic)
+------------------------------------------------------------
+local function GetBNetFriends()
+    return {}
+end
 
+------------------------------------------------------------
+-- Collect custom friends (manual whitelist)
+------------------------------------------------------------
+local function GetCustomFriends()
+    local friends = {}
+    if FFAPartyDB and FFAPartyDB.customFriends then
+        for _, name in ipairs(FFAPartyDB.customFriends) do
+            friends[NormalizeName(name)] = true
+        end
+    end
+    return friends
+end
+
+------------------------------------------------------------
+-- Collect current group members
+------------------------------------------------------------
+local function GetGroupMembers()
+    local members = {}
+    local num = GetNumGroupMembers()
+    if num > 0 then
+        local prefix = IsInRaid() and "raid" or "party"
+        for i = 1, num do
+            local unit = prefix .. i
+            if UnitExists(unit) then
+                local name = UnitName(unit)
+                if name then
+                    table.insert(members, NormalizeName(name))
+                end
+            end
+        end
+        table.insert(members, NormalizeName(UnitName("player")))
+    end
+    return members
+end
+
+------------------------------------------------------------
+-- Check if group is only friends
+------------------------------------------------------------
+local function IsGroupOnlyFriends()
+    local friends = {}
+    for k in pairs(GetWoWFriends()) do
+        friends[k] = true
+    end
+    for k in pairs(GetBNetFriends()) do
+        friends[k] = true
+    end
+    for k in pairs(GetCustomFriends()) do
+        friends[k] = true
+    end
+
+    for _, member in ipairs(GetGroupMembers()) do
+        -- Skip the player themselves
+        if member ~= NormalizeName(UnitName("player")) then
+            if not friends[member] then
+                DebugPrint("Non-friend detected in group: " .. (member or "unknown"))
+                return false
+            else
+                DebugPrint("Friend matched: " .. (member or "unknown"))
+            end
+        end
+    end
+
+    return true
+end
+
+------------------------------------------------------------
+-- Enforce loot method
+------------------------------------------------------------
+local function UpdateLootMethod()
+    if not IsInGroup() then
+        return
+    end
+    if not UnitIsGroupLeader("player") then
+        return
+    end
+    if IsInRaid() and FFAPartyDB.ignoreRaids then
+        DebugPrint("In raid, ignoring loot enforcement")
         return
     end
 
-    ensure_loot_method(LOOT_TYPE_FFA)
+    local desiredLootKey = IsGroupOnlyFriends() and FFAPartyDB.lootWithFriends or FFAPartyDB.lootWithOthers
+    local desiredLoot = lootMethodMap[desiredLootKey]
 
-    -- print(currentCharacterName)
-    -- SetOptOutOfLoot(true)
-    -- print(event, addOnName)
+    local currentMethod = nil
+    if C_PartyInfo and C_PartyInfo.GetLootMethod then
+        currentMethod = C_PartyInfo.GetLootMethod()
+    elseif GetLootMethod then
+        currentMethod = GetLootMethod()
+    end
+
+    if currentMethod ~= desiredLoot then
+        if C_PartyInfo and C_PartyInfo.SetLootMethod then
+            C_PartyInfo.SetLootMethod(desiredLoot)
+        elseif SetLootMethod then
+            SetLootMethod(desiredLoot)
+        end
+
+        if FFAPartyDB.showMessages then
+            print("Loot set to " .. desiredLootKey)
+        end
+        DebugPrint("Loot method updated to " .. desiredLootKey)
+    else
+        DebugPrint("Loot method already " .. desiredLootKey .. ", no change")
+    end
 end
 
-function f:GOSSIP_SHOW(event, addOnName)
-    ensure_raid_target(MY_CHAR_UNIT, MY_RAID_TARGET_INDEX)
-    ensure_raid_target(OTHER_CHAR_UNIT, OTHER_RAID_TARGET_INDEX)
+------------------------------------------------------------
+-- Event handling
+------------------------------------------------------------
+function f:OnEvent(event, ...)
+    if event == "ADDON_LOADED" then
+        local loadedAddon = ...
+        if loadedAddon == addonName then
+            if not FFAPartyDB then
+                FFAPartyDB = {}
+            end
+            for k, v in pairs(defaults) do
+                if FFAPartyDB[k] == nil then
+                    FFAPartyDB[k] = v
+                end
+            end
 
-    process_group_loot_type()
+            print("FFA Party: about to call CreateOptionsPanel; value is", addon.CreateOptionsPanel)
+            if addon.CreateOptionsPanel then
+                addon.CreateOptionsPanel()
+            else
+                print("FFA Party: CreateOptionsPanel is nil at ADDON_LOADED")
+            end
+            DebugPrint("FFA Party loaded and options initialized")
+        end
+    elseif event == "GROUP_ROSTER_UPDATE" or event == "PARTY_LEADER_CHANGED" or event == "PLAYER_ENTERING_WORLD" then
+        UpdateLootMethod()
+    end
 end
 
-function f:GROUP_ROSTER_UPDATE()
-    ensure_raid_target(MY_CHAR_UNIT, MY_RAID_TARGET_INDEX)
-    ensure_raid_target(OTHER_CHAR_UNIT, OTHER_RAID_TARGET_INDEX)
-
-    process_group_loot_type()
-end
-
-
-
--- f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:RegisterEvent("ADDON_LOADED")
--- f:RegisterEvent("GOSSIP_SHOW")
 f:RegisterEvent("GROUP_ROSTER_UPDATE")
+f:RegisterEvent("PARTY_LEADER_CHANGED")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", f.OnEvent)
--- options.lua
+++ options.lua
@@ -1,19 +1,126 @@
--- local name,addon=...;
--- print(addon.myvar)
+-- options.lua
+local addonName, addon = ...
+addon = addon or {}
+_G[addonName] = addon
+print("FFA Party: options.lua loaded for", addonName, "addon table:", addon)
 
-local panel = CreateFrame("Frame")
-panel.name = "FFA Party"
+function addon.CreateOptionsPanel()
+    local panel = CreateFrame("Frame")
+    panel.name = "FFA Party" -- force a nice display name
 
-local title = panel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
-title:SetPoint("TOP")
-title:SetText("FFA Party")
+    local title = panel:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+    title:SetPoint("TOPLEFT", 16, -16)
+    title:SetText("FFA Party Options")
 
-InterfaceOptions_AddCategory(panel)
+    --------------------------------------------------------
+    -- Ignore raids checkbox
+    --------------------------------------------------------
+    local ignoreRaidsCB = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
+    ignoreRaidsCB:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -10)
+    ignoreRaidsCB.Text:SetText("Ignore raids")
+    ignoreRaidsCB:SetChecked(FFAPartyDB.ignoreRaids)
+    ignoreRaidsCB:SetScript("OnClick", function(self)
+        FFAPartyDB.ignoreRaids = self:GetChecked()
+    end)
 
-SLASH_FFAPARTY1 = "/fp"
-SLASH_FFAPARTY2 = "/ffap"
-SLASH_FFAPARTY3 = "/ffaparty"
+    --------------------------------------------------------
+    -- Show messages checkbox
+    --------------------------------------------------------
+    local showMessagesCB = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
+    showMessagesCB:SetPoint("TOPLEFT", ignoreRaidsCB, "BOTTOMLEFT", 0, -10)
+    showMessagesCB.Text:SetText("Show chat messages")
+    showMessagesCB:SetChecked(FFAPartyDB.showMessages)
+    showMessagesCB:SetScript("OnClick", function(self)
+        FFAPartyDB.showMessages = self:GetChecked()
+    end)
 
-SlashCmdList.FFAPARTY = function(msg, editBox)
-	InterfaceOptionsFrame_OpenToCategory(panel)
+    --------------------------------------------------------
+    -- Debug mode checkbox
+    --------------------------------------------------------
+    local debugCB = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
+    debugCB:SetPoint("TOPLEFT", showMessagesCB, "BOTTOMLEFT", 0, -10)
+    debugCB.Text:SetText("Enable debug logging")
+    debugCB:SetChecked(FFAPartyDB.debug)
+    debugCB:SetScript("OnClick", function(self)
+        FFAPartyDB.debug = self:GetChecked()
+    end)
+
+    --------------------------------------------------------
+    -- Loot with friends dropdown
+    --------------------------------------------------------
+    local lootFriendsLabel = panel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    lootFriendsLabel:SetPoint("TOPLEFT", debugCB, "BOTTOMLEFT", 0, -20)
+    lootFriendsLabel:SetText("Loot with friends:")
+
+    local lootFriendsDD = CreateFrame("Frame", addonName .. "LootFriendsDD", panel, "UIDropDownMenuTemplate")
+    lootFriendsDD:SetPoint("TOPLEFT", lootFriendsLabel, "BOTTOMLEFT", -16, -5)
+
+    local lootMethods = {{
+        value = "freeforall",
+        text = "Free-for-all"
+    }, {
+        value = "group",
+        text = "Group Loot"
+    }, {
+        value = "needbeforegreed",
+        text = "Need Before Greed"
+    }}
+
+    UIDropDownMenu_SetWidth(lootFriendsDD, 150)
+    UIDropDownMenu_Initialize(lootFriendsDD, function(self, level)
+        for _, option in ipairs(lootMethods) do
+            local info = UIDropDownMenu_CreateInfo()
+            info.text = option.text
+            info.func = function()
+                FFAPartyDB.lootWithFriends = option.value
+                UIDropDownMenu_SetSelectedValue(lootFriendsDD, option.value)
+            end
+            UIDropDownMenu_AddButton(info)
+        end
+    end)
+    UIDropDownMenu_SetSelectedValue(lootFriendsDD, FFAPartyDB.lootWithFriends)
+
+    --------------------------------------------------------
+    -- Loot with others dropdown
+    --------------------------------------------------------
+    local lootOthersLabel = panel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    lootOthersLabel:SetPoint("TOPLEFT", lootFriendsDD, "BOTTOMLEFT", 16, -20)
+    lootOthersLabel:SetText("Loot with others:")
+
+    local lootOthersDD = CreateFrame("Frame", addonName .. "LootOthersDD", panel, "UIDropDownMenuTemplate")
+    lootOthersDD:SetPoint("TOPLEFT", lootOthersLabel, "BOTTOMLEFT", -16, -5)
+
+    UIDropDownMenu_SetWidth(lootOthersDD, 150)
+    UIDropDownMenu_Initialize(lootOthersDD, function(self, level)
+        for _, option in ipairs(lootMethods) do
+            local info = UIDropDownMenu_CreateInfo()
+            info.text = option.text
+            info.func = function()
+                FFAPartyDB.lootWithOthers = option.value
+                UIDropDownMenu_SetSelectedValue(lootOthersDD, option.value)
+            end
+            UIDropDownMenu_AddButton(info)
+        end
+    end)
+    UIDropDownMenu_SetSelectedValue(lootOthersDD, FFAPartyDB.lootWithOthers)
+
+    --------------------------------------------------------
+    -- Register panel
+    --------------------------------------------------------
+    if InterfaceOptions_AddCategory then
+        InterfaceOptions_AddCategory(panel)
+    end
+
+    --------------------------------------------------------
+    -- Slash commands to open panel
+    --------------------------------------------------------
+    SLASH_FFAPARTY1 = "/fp"
+    SLASH_FFAPARTY2 = "/ffap"
+    SLASH_FFAPARTY3 = "/ffaparty"
+    SlashCmdList.FFAPARTY = function()
+        if InterfaceOptionsFrame then
+            InterfaceOptionsFrame:Show()
+            print("Open Interface Options → AddOns → FFA Party to configure settings.")
+        end
+    end
 end
